313a3f02e101523cd6a9b404ee3bfaf44fcb0ef6
Akhil Pothineni <likhapothineni@gmail.com>
2020-05-22 17:38:34 -0700

    'chrome.storage' bug fixes, style changes

--- popup.js
+++ popup.js
@@ -7,22 +7,20 @@
 const tableData = ['bool house', 'liggy', 'thoom', 'shim ravage'];
 
 document.addEventListener('DOMContentLoaded', function(){
-    document.addEventListener('DOMContentLoaded', function(){
-        document.getElementById('enterButton').onclick = function () {
-            chrome.storage.sync.get('myLine', function(data){
-                alert(data.myLine);
-            });
-            // navigator.clipboard.writeText(value);
-            //copies the value in the input field to the clipboard
-        }
-        document.getElementById('save').onclick = function () {
-            var value = document.getElementById('saveLine').value;
-            chrome.storage.sync.set({'myLine': value}, function(){
-                alert('Success');
-            });
-            // alert(value);
-        }
-    })
+    document.getElementById('boolHouse').onclick = function () {
+        chrome.storage.sync.get('myLine', function(data){
+            navigator.clipboard.writeText(data.myLine);
+            // alert(data.myLine);
+        });
+        //copies the value in the input field to the clipboard
+    }
+    document.getElementById('save').onclick = function () {
+        var value = document.getElementById('saveLine').value;
+        chrome.storage.sync.set({'myLine': value}, function(){
+            // alert('Success');
+        });
+        // alert(value);
+    }
 })
 //displays a list of the things in the array, need to make it so that the array is stored using the chrome.storage API
 function makeList(){ 
@@ -42,7 +40,7 @@ function makeList(){
         listElement.appendChild(listItem);        
     }
 }
-// makeList();
+makeList();
 //similar to the function above but displays the list in the form of a table
 function makeTable(){ 
     var table = document.createElement('table');
